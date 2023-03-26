ee104f639d3e50e311d9b8e0ad706f15988fcb2a
Brunvelop <brunvelop@gmail.com>
2023-03-26 19:19:23 +0200

    style: split code in components

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,42 @@ import Image from "next/image";
 import styles from '../styles/Home.module.css'
 import { useState } from 'react';
 
-const Home = () => {
+const HeadComponent = () => {
+  return (
+    <Head>
+      <title>Typeart</title>
+      <meta name="description" content="Stable Diffusion Bitcoin" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
 
+const Banner = () => {
+  return (
+    <div className={styles.banner}>
+      <p>🚨 Attention: Due to high traffic, our servers may have slower performance.</p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://www.banana.dev/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <span className={styles.logo}>
+          <Image src="/banana.svg" alt="Banana Logo" width={72} height={16} />
+        </span>
+      </a>
+    </footer>
+  );
+};
+
+const SDGenerator = () => {
   const [prompt, setPrompt] = useState("");
   const [image, setImage] = useState();
   const [loading, setLoading] = useState(false);
@@ -27,28 +61,17 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Typeart</title>
-        <meta name="description" content="Stable Diffusion Bitcoin" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Stable Diffusion
+      </h1>
 
-      <div className = {styles.banner}>
-        <p>🚨 Attention: Due to high traffic, our servers may have slower performance.</p>
-      </div>
+      {image ? <div className = {styles.image}><Image src = {`data:image/png;base64,${image}`} alt = "Generated image" layout = "fill" objectFit = "cover"></Image> </div> 
+      : <p className={styles.description}> Enter a prompt to generate an image. </p>}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Stable Diffusion
-        </h1>
+      {loading ? <p>Loading... please wait up to a minute.</p> : null}
 
-        {image ? <div className = {styles.image}><Image src = {`data:image/png;base64,${image}`} alt = "Generated image" layout = "fill" objectFit = "cover"></Image> </div> 
-        : <p className={styles.description}> Enter a prompt to generate an image. </p>}
-
-        {loading ? <p>Loading... please wait up to a minute.</p> : null}
-
-        <div className={styles.diffusion}>
+      <div className={styles.diffusion}>
         <form onSubmit = {handleSubmit}>
           
           <textarea 
@@ -64,23 +87,21 @@ const Home = () => {
           
           <div className = {styles.generatebuttonroot}>
             <button type = "submit" className = {styles.generatebutton}>Generate</button>
-            </div>
-            </form>
-            </div>
-            </main>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
 
-      <footer className={styles.footer}>
-        <a
-          href="https://www.banana.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/banana.svg" alt="Banana Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <HeadComponent/>
+      <Banner/>
+      <SDGenerator/>
+      <Footer/>
     </div>
   )
 }
