1250a940c6aa19d56296c8b745d24ebc728b5316
Brunvelop <brunvelop@gmail.com>
2023-03-28 12:08:46 +0200

    feat: improve inscribe api

--- pages/api/inscribe.js
+++ pages/api/inscribe.js
@@ -1,34 +1,58 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'https://generative.xyz/generative/api/developer';
+const API_BASE_URL = "https://generative.xyz/generative/api/developer";
 const API_KEY = process.env.GENERATIVE_API_KEY;
 
-export default async function handler(req, res) {
+// Helper function to create payment address
+async function createInscription(walletAddress, fileName, feeRate, fileData) {
   const url = `${API_BASE_URL}/inscribe?api-key=${API_KEY}`;
 
-  const walletAddress = req.body.walletAddress;
-  const fileName = "photo.jpeg";
-  const feeRate = 15;
-  const fileData = "data:image/jpeg;base64,"+req.body.fileBase64;
-
-  try {
-    const response = await axios.post(url, {
-      walletAddress: walletAddress,
-      fileName:fileName,
-      feeRate:feeRate,
+  const response = await axios.post(
+    url,
+    {
+      walletAddress,
+      fileName,
+      feeRate,
       file: fileData,
-    }, {
+    },
+    {
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        "Content-Type": "application/json",
+        "Accept": "application/json",
       },
-    });
+    }
+  );
 
-    res.status(200).json(response.data);
-
-  } catch (error) {
-    console.error('Error getting payment address:', error);
-    res.status(error.response.status || 500).json({ message: error.message });
-  }
+  return response.data;
 }
 
+// Main function to handle requests and responses
+export default async function handler(req, res) {
+  try {
+    // Get wallet address and file data from request body
+    const walletAddress = req.body.walletAddress;
+    const fileBase64 = req.body.fileBase64;
+
+    // Validate input
+    if (!walletAddress || !fileBase64) {
+      res
+        .status(400)
+        .json({ error: "Missing wallet address or file data in the request body" });
+      return;
+    }
+
+    // Set constants
+    const fileName = "photo.jpeg";
+    const feeRate = 9;
+    const fileData = `data:image/jpeg;base64,${fileBase64}`;
+
+    const data = await createInscription(walletAddress, fileName, feeRate, fileData);
+
+    // Send payment address data as JSON response
+    res.status(200).json(data);
+  } catch (error) {
+    // Handle errors and exceptions
+    console.error("Error getting payment address:", error);
+    res.status(error.response?.status || 500).json({ message: error.message });
+  }
+}
