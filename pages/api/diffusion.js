7a9dc70d216c17be207a0cfac939c7d06fa281bf
Brunvelop <brunvelop@gmail.com>
2023-03-28 11:59:04 +0200

    Style: Improve difussion api

--- pages/api/diffusion.js
+++ pages/api/diffusion.js
@@ -1,20 +1,41 @@
 import * as banana from "@banana-dev/banana-dev";
 
-// Enter your Banana API keys in .env.local
-const apiKey = process.env.BANANA_API_KEY
-const modelKey = process.env.BANANA_MODEL_KEY
+// Load Banana API keys from environment variables
+const apiKey = process.env.BANANA_API_KEY;
+const modelKey = process.env.BANANA_MODEL_KEY;
 
+// Helper function to run Banana model
+async function runBananaModel(prompt, height = 64, width = 64, steps = 20) {
+  const modelParameters = {
+    prompt,
+    height,
+    width,
+    steps,
+  };
+
+  return await banana.run(apiKey, modelKey, modelParameters);
+}
+
+// Main function to handle requests and responses
 export default async function (req, res) {
+  try {
+    // Get prompt from request body
+    const prompt = req.body.prompt;
 
-    const modelParameters = {
-        "prompt": req.body.prompt,
-        "height": 384,
-        "width": 384,
-        "steps": 20,
+    // Validate input
+    if (!prompt) {
+      res.status(400).json({ error: "Missing prompt in the request body" });
+      return;
     }
 
-    const output = await banana.run(apiKey, modelKey, modelParameters);
+    // Run model and get output
+    const output = await runBananaModel(prompt);
 
+    // Send output as JSON response
     res.status(200).json(output);
-
+  } catch (error) {
+    // Handle errors and exceptions
+    console.error("Error running Banana model:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 }
