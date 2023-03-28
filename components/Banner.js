417a717d50e6650ba9821688c3ce435df04f3850
Brunvelop <brunvelop@gmail.com>
2023-03-28 16:39:03 +0200

    Refactor: Extract Baner and SDGenerator component

---
+++ components/Banner.js
@@ -0,0 +1,8 @@
+const Banner = () => {
+    return (
+      <div className="w-full absolute top-0 text-center font-medium">
+        <p>🚨 Attention: Due to high traffic, our servers may have slower performance.</p>
+      </div>
+    );
+  };
+  
