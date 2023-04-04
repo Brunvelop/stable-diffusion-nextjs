bbffedac759f81a3b39c75d260a4801fc91078ce
Brunvelop <brunvelop@gmail.com>
2023-04-04 11:08:30 +0200

    feat: Minimal version xverse transaction working on testnet

--- pages/xverse.js
+++ pages/xverse.js
@@ -4,10 +4,27 @@ import * as btc from 'micro-btc-signer';
 import { hex, base64 } from '@scure/base';
 import axios from "axios";
 
-// Function to get unspent outputs for a given address
+const TESTNET = true;
+
+const API_URL = TESTNET
+  ? 'https://blockstream.info/testnet/api'
+  : 'https://blockstream.info/api';
+
+const MEMPOOL_API_URL = TESTNET
+  ? 'https://mempool.space/testnet/api'
+  : 'https://mempool.space/api';
+
+const BITCOIN_NETWORK = {
+  bech32: TESTNET ? 'tb' : 'bc',
+  pubKeyHash: TESTNET ? 0x6f : 0x00,
+  scriptHash: TESTNET ? 0xc4 : 0x05,
+  wif: TESTNET ? 0xef : 0x80,
+};
+
 async function getUnspentOutputs(address) {
   try {
-    const response = await axios.get(`https://blockstream.info/testnet/api/address/${address}/utxo`);
+    const url = `${API_URL}/address/${address}/utxo`;
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
     console.error('Error fetching UTXOs:', error);
@@ -15,57 +32,83 @@ async function getUnspentOutputs(address) {
   }
 }
 
-// Function to create a PSBT for a given user address and public key
-async function createPstb(userAddress, userPublicKey)  {
-  const bitcoinTestnet = {
-    bech32: 'tb',
-    pubKeyHash: 0x6f,
-    scriptHash: 0xc4,
-    wif: 0xef,
-  };
+async function getAddressUtxos(address) {
+  const url = `${MEMPOOL_API_URL}/address/${address}/utxo`;
+  const response = await axios.get(url);
+  return response.data;
+}
 
-  // Replace this with the address for which you want to get UTXOs
+async function doesUtxoContainInscription(utxo) {
+  const html = await fetch(`https://ordinals.com/output/${utxo.txid}:${utxo.vout}`)
+    .then(response => response.text());
+
+  return html.match(/class=thumbnails/) !== null;
+}
+
+async function selectUtxos(utxos, amount) {
+  const selectedUtxos = [];
+  let selectedAmount = 0;
+
+  for (const utxo of utxos) {
+    if (await doesUtxoContainInscription(utxo)) {
+      continue;
+    }
+    selectedUtxos.push(utxo);
+    selectedAmount += utxo.value;
+    
+    if (selectedAmount >= amount) {
+      break;
+    }
+  }
+  
+
+  if (selectedAmount < amount) {
+    throw new Error(`Not enough spendable funds.
+Address has:  ${satToBtc(selectedAmount)} BTC
+Needed:          ${satToBtc(amount)} BTC
+
+UTXOs:
+${utxos.map(x => `${x.txid}:${x.vout}`).join("\n")}`);
+  }
+
+  return selectedUtxos;
+}
+
+async function createPstb(userAddress, userPublicKey) {
   const utxos = await getUnspentOutputs(userAddress);
-  console.log('UTXOs:', utxos);
-
-  // You can use any public Bitcoin API to get the unspent outputs
-  const output = {
-    tx_hash: utxos[0].txid,
-    tx_output_n: utxos[0].vout,
-    value: utxos[0].value,
-  };
-  console.log('Output:', output);
+  const selectedUtxos = await selectUtxos(utxos, 1000);
 
   const publicKey = hex.decode(userPublicKey);
-  const p2wpkh = btc.p2wpkh(publicKey, bitcoinTestnet);
-  const p2sh = btc.p2sh(p2wpkh, bitcoinTestnet);
+  const p2wpkh = btc.p2wpkh(publicKey, BITCOIN_NETWORK);
+  const p2sh = btc.p2sh(p2wpkh, BITCOIN_NETWORK);
 
-  const tx = new btc.Transaction(bitcoinTestnet);
+  const tx = new btc.Transaction(BITCOIN_NETWORK);
 
-  tx.addInput({
-    txid: output.tx_hash,
-    index: output.tx_output_n,
-    witnessUtxo: {
-      script: p2sh.script,
-      amount: BigInt(output.value),
-    },
-    redeemScript: p2sh.redeemScript,
+  selectedUtxos.forEach(utxo => {
+    tx.addInput({
+      txid: utxo.txid,
+      index: utxo.vout,
+      witnessUtxo: {
+        script: p2sh.script,
+        amount: BigInt(utxo.value),
+      },
+      redeemScript: p2sh.redeemScript,
+    });
   });
 
-  // add more inputs here if needed
   // Add outputs
   const recipient = "tb1qvxsq3fs30hxmul5rycnflfj30kwjygw8r7qmh6";
   const changeAddress = userAddress;
 
-  tx.addOutputAddress(recipient, BigInt(200), bitcoinTestnet);
-  tx.addOutputAddress(recipient, BigInt(100), bitcoinTestnet);
-  tx.addOutputAddress(changeAddress, BigInt(1500), bitcoinTestnet);
+  tx.addOutputAddress(recipient, BigInt(200), BITCOIN_NETWORK);
+  tx.addOutputAddress(recipient, BigInt(100), BITCOIN_NETWORK);
+  tx.addOutputAddress(changeAddress, BigInt(500), BITCOIN_NETWORK);
 
   // Generate the base64-encoded PSBT that can be
   // passed to a compatible wallet for signing
   const psbt = tx.toPSBT(0);
   const psbtB64 = base64.encode(psbt);
-  return psbtB64
+  return psbtB64;
 }
 
 export default function Xverse() {
@@ -86,9 +129,8 @@ export default function Xverse() {
       },
     },
     onFinish: (response) => {
-      console.log(response);
       setUserAddress(response.addresses[1].address);
-      setPublicKey(response.addresses[1].publicKey)
+      setPublicKey(response.addresses[1].publicKey);
     },
     onCancel: () => alert('Request canceled'),
   };
@@ -96,39 +138,34 @@ export default function Xverse() {
   const handleConnectButtonClick = useCallback(async () => {
     await getAddress(getAddressOptions);
   }, []);
-    
+
   const handleSignTransactionButtonClick = useCallback(async () => {
-    // Build the PSBT using a suitable Bitcoin library (e.g. bitcoinjs-lib)
-    // Make sure to include the destination addresses and desired amounts.
-    const psbtBase64 = await createPstb(userAddress, publicKey); // The base64-encoded PSBT
-    console.log('psbtBase64', psbtBase64)
-    console.log('User addrs', userAddress)
+    const psbtBase64 = await createPstb(userAddress, publicKey);
+    console.log('psbtBase64', psbtBase64);
 
     const signTransactionOptions = {
-        payload: {
-          network: {
-            type: 'Testnet',
+      payload: {
+        network: {
+          type: 'Testnet',
+        },
+        message: 'Sign Transaction',
+        psbtBase64,
+        broadcast: true,
+        inputsToSign: [
+          {
+            address: userAddress,
+            signingIndexes: [0],
           },
-          message: 'Sign Transaction',
-          psbtBase64,
-          broadcast: true,
-          inputsToSign: [
-            {
-              // Make sure to provide the correct address
-              // and signing indices for the user's address in the transaction
-              address: userAddress, // Replace with the user's address
-              signingIndexes: [0], // Change as needed
-            },
-          ],
-        },
-        onFinish: (response) => {
-          console.log(response.txid); // Show the TXID of the transmitted transaction
-        },
-        onCancel: () => alert('Transaction signing canceled'),
-      };
-      
-      await signTransaction(signTransactionOptions);
-    }, [userAddress, publicKey]);
+        ],
+      },
+      onFinish: (response) => {
+        console.log(response.txid);
+      },
+      onCancel: () => alert('Transaction signing canceled'),
+    };
+
+    await signTransaction(signTransactionOptions);
+  }, [userAddress, publicKey]);
 
   return (
     <div className="inset-0 flex items-center justify-center m-20">
