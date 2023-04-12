b28c8875bc53182567efb976c1d615bd7f1dc85a
Brunvelop <brunvelop@gmail.com>
2023-04-12 18:17:07 +0200

    feat: Update home with xverse integration

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo, useCallback } from 'react';
 
 import Footer from "../components/Footer";
 import UserWalletInfo from "../components/UserWalletInfo";
 import HeadComponent from "../components/HeadComponent";
 import Banner from "../components/Banner";
 import SDGenerator from "../components/SDGenerator";
-import YellowButton from "../components/YellowButton";
+import YellowButton from '../components/YellowButton';
+import Wallet from "../components/wallet";
+import XverseProvider from "../components/wallet/XverseProvider";
 import Gallery from "../components/Gallery";
 
 const Home = () => {
   const [connected, setConnected] = useState(false);
-  const [address, setAddress] = useState("");
-  const [balance, setBalance] = useState({
-    confirmed: 0,
-    unconfirmed: 0,
-    total: 0,
-  });
+  const [reciveAddress, setReciveAddress] = useState('');
+  const [paymentAddress, setPaymentAddress] = useState('');
+  const [connecting, setConnecting] = useState(false);
+  const [balance, setBalance] = useState({ confirmed: 0 });
 
-  const handleAccountsChanged = async (accounts) => {
-    if (accounts.length > 0) {
+  const xverseProvider = useMemo(() => new XverseProvider('Mainnet'), []);
+  const wallet = useMemo(() => new Wallet(xverseProvider), [xverseProvider]);
+
+  const handleConnectWallet = useCallback(async () => {
+    setConnecting(true);
+    try {
+      const info = await wallet.getInfo();
+      setPaymentAddress(info.paymentAddress);
+      setReciveAddress(info.receivingAddress);
+      setBalance({ confirmed: info.balance });
       setConnected(true);
-      getBasicInfo(window.unisat);
-    } else {
+    } catch (error) {
+      console.error('Error connecting to wallet:', error);
       setConnected(false);
+    } finally {
+      setConnecting(false);
     }
-  };
-
-  const getBasicInfo = async (unisat) => {
-    const [account] = await unisat.getAccounts();
-    setAddress(account);
-
-    const balance = await unisat.getBalance();
-    setBalance(balance);
-  };
-
-  useEffect(() => {
-    const unisat = window.unisat;
-    if (unisat) {
-      unisat.getAccounts().then(handleAccountsChanged);
-      unisat.on("accountsChanged", handleAccountsChanged);
-
-      return () => {
-        unisat.removeListener("accountsChanged", handleAccountsChanged);
-      };
-    }
-  }, []);
-
+  }, [wallet]);
+  
   return (
     <div className="flex flex-col items-center min-h-screen">
       <HeadComponent />
-      {/* <Banner/> */}
       {connected ? (
         <div className="flex flex-col items-center justify-center flex-grow">
-          <UserWalletInfo address={address} balanceSats={balance.confirmed} />
-          <SDGenerator address={address} />
-          <Gallery />
+          <UserWalletInfo address={paymentAddress} balanceSats={balance.confirmed} />
+          <SDGenerator address={reciveAddress} wallet={wallet}/>
+          <Gallery/>
         </div>
       ) : (
-        <div className="flex flex-grow items-center justify-center">
-          <YellowButton
-            onClick={async () => {
-              const result = await window.unisat.requestAccounts();
-              handleAccountsChanged(result);
-            }}
-          >
-            Connect Wallet
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <div className="m-20">
+          <YellowButton onClick={handleConnectWallet} disabled={connecting}>
+            {connecting ? 'Connecting...' : 'Connect Xverse'}
           </YellowButton>
+          </div>
+          <Gallery/>
         </div>
       )}
       <Footer />
