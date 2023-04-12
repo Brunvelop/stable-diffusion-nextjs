9dbe9a09843deb8e027b1bac4d73f65e4ceca125
Brunvelop <brunvelop@gmail.com>
2023-04-12 11:09:15 +0200

    feat: Create inscriptions endpoint

---
+++ pages/api/inscriptions.js
@@ -0,0 +1,38 @@
+// api/inscriptions.js
+
+import { createClient } from '@supabase/supabase-js';
+
+// Utiliza las variables de entorno para crear el cliente supabase
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+export default async function handler(req, res) {
+  // Verifica si el método de la solicitud es POST
+  if (req.method === 'POST') {
+    // Extrae los datos necesarios del cuerpo de la solicitud
+    const { recive_address, id_generative, tx_pay, image_base64 } = req.body;
+
+    // Inserta los datos en la tabla 'purchases'
+    const { data, error } = await supabase.from('purchases').insert([
+      {
+        recive_address,
+        id_generative,
+        tx_pay,
+        tx_pay_confirmed: false,
+        image_base64,
+      },
+    ]);
+
+    // Si hay un error, envía una respuesta con el error
+    if (error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      // Si todo está bien, envía una respuesta con los datos insertados
+      res.status(200).json(data);
+    }
+  } else {
+    // Si el método de la solicitud no es POST, envía un mensaje de error
+    res.status(405).json({ error: 'Método no permitido' });
+  }
+}
