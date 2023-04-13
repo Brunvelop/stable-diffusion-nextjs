02e1998c1aeff7ef437835f37560cfe1325aa448
Brunvelop <brunvelop@gmail.com>
2023-04-13 21:34:25 +0200

    Fix: rel="noopener noreferrer" security issue

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer className="w-full fixed bottom-0 text-center font-medium py-4 flex justify-between items-center">
       <div className="flex justify-center">
-        <a href="https://twitter.com/TypeartOrg" target="_blank">
+        <a href="https://twitter.com/TypeartOrg" target="_blank" rel="noopener noreferrer">
           <FaTwitter className="text-yellow-300 mx-2" size='1.5rem' />
         </a>
-        <a href="https://discord.gg/6j4TV2fegV" target="_blank">
+        <a href="https://discord.gg/6j4TV2fegV" target="_blank" rel="noopener noreferrer">
           <FaDiscord className="text-yellow-300 mx-2" size='1.5rem' />
         </a>
         
