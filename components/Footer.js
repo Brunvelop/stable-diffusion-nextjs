2566b07cf28ff530518917bb6215e1c0f783a84e
Brunvelop <brunvelop@gmail.com>
2023-04-13 18:43:08 +0200

    feat: update footer links

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,13 @@ const Footer = () => {
   return (
     <footer className="w-full fixed bottom-0 text-center font-medium py-4 flex justify-between items-center">
       <div className="flex justify-center">
-        <FaTwitter className="text-yellow-300 mx-2" size='1.5rem' />
-        <FaDiscord className="text-yellow-300 mx-2" size='1.5rem' />
+        <a href="https://twitter.com/TypeartOrg" target="_blank">
+          <FaTwitter className="text-yellow-300 mx-2" size='1.5rem' />
+        </a>
+        <a href="https://discord.gg/6j4TV2fegV" target="_blank">
+          <FaDiscord className="text-yellow-300 mx-2" size='1.5rem' />
+        </a>
+        
       </div>
       <div className="flex">
         <a
